a4c6c5a27106cab24d5d8efce02f398087d1c0d4
CypriotUnknown <49270120+CypriotUnknown@users.noreply.github.com>
2024-03-25 15:44:18 +0200

    added local ip functionality

--- ts/index.ts
+++ ts/index.ts
@@ -1,15 +1,20 @@
 import playwright from 'playwright'
 import SecretsManager from './secrets-manager.js'
 import { AxiosError } from 'axios';
-
+import os from 'os';
+import { exit } from 'process';
 
 async function start() {
+    const localIp = getLocalIp()
     await SecretsManager.instance.setupAccessTokens();
 
     try {
         const r = await playwright.chromium.launchServer(SecretsManager.instance.serverConfiguration)
 
-        await SecretsManager.instance.updateBrowserURL(r.wsEndpoint())
+        const browserURL = r.wsEndpoint().replace("localhost", localIp);
+        await SecretsManager.instance.updateBrowserURL(browserURL);
+
+        console.log(`Started browser server at: ${browserURL}`);
     } catch (error) {
         if (error instanceof AxiosError) {
             console.log(error.response?.data.message);
@@ -19,5 +24,23 @@ async function start() {
     }
 }
 
+function getLocalIp(): string {
+    try {
+        // Get the network interfaces
+        const networkInterfaces = os.networkInterfaces();
+
+        // Find the IPv4 address of the local network interface
+        const localIpAddress = Object.values(networkInterfaces)
+            .flat()
+            .filter(interfaceInfo => interfaceInfo!.family === 'IPv4' && !interfaceInfo!.internal)
+            .map(interfaceInfo => interfaceInfo!.address)
+            .find(Boolean); // Find the first non-empty IPv4 address
+
+        return localIpAddress!;
+    } catch (error) {
+        console.log('Failed to get local ip address !\n', error);
+        exit(1);
+    }
+}
 
 start()
